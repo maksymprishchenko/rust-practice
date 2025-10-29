5ab7ff2f74d940b0591f6f0030d2fb9c2b274ea9
maksymprishchenko <maksym.prishchenko@e-u.edu.ua>
2025-10-30 00:40:26 +0200

    Refactor main function for better readability

--- src/homework/hw03.rs
+++ src/homework/hw03.rs
@@ -1,12 +1,16 @@
 const WIDTH: usize = 15;
 const HEIGHT: usize = 15;
 
-fn main() { 
+fn main() {
     for y in 0..HEIGHT {
         for x in 0..WIDTH {
-            if y == 0  y == HEIGHT - 1  x == 0 || x == WIDTH - 1
-                || x == y
-                || x == (WIDTH - 1) - y
+            if
+                y == 0 ||
+                y == HEIGHT - 1 ||
+                x == 0 ||
+                x == WIDTH - 1 ||
+                x == y ||
+                x == (WIDTH - 1) - y
             {
                 print!("*");
             } else {
