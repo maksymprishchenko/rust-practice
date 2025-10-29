166c9f8df8a8edc68354d6f0ef9df641b6c9999a
mac <mac@MacBook-Air-mac.lan>
2025-10-29 23:07:27 +0200

    hw03: add envelope implementation

---
+++ src/homework/hw03.rs
@@ -0,0 +1,18 @@
+const WIDTH: usize = 15;
+const HEIGHT: usize = 15;
+
+fn main() {
+    for y in 0..HEIGHT {
+        for x in 0..WIDTH {
+            if y == 0  y == HEIGHT - 1  x == 0 || x == WIDTH - 1
+                || x == y
+                || x == (WIDTH - 1) - y
+            {
+                print!("*");
+            } else {
+                print!(" ");
+            }
+        }
+        println!();
+    }
+}
